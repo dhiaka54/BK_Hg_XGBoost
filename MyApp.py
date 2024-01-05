1b4b693ddd2a9a659f680c42cf493a0cdeefcb2a
dhiaka54 <111202013094@mhs.dinus.ac.id>
2024-01-05 14:13:42 +0700

    1

--- MyApp.py
+++ MyApp.py
@@ -86,8 +86,8 @@ y = df_clean['target']
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
-# model = pickle.load(open("model/xgb_model.pkl", 'rb'))
-model = pickle.load(open("model/modelXGB_rcv.pkl", 'rb'))
+model = pickle.load(open("model/xgb_model.pkl", 'rb'))
+# model = pickle.load(open("model/modelXGB_rcv.pkl", 'rb'))
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
