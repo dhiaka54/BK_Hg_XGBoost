e3be921b655622d291a09cb9c3e3c12a8577eec4
dhiaka54 <111202013094@mhs.dinus.ac.id>
2024-01-05 20:03:12 +0700

    a

--- MyApp.py
+++ MyApp.py
@@ -111,6 +111,8 @@ tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
 
 with tab1:
   st.sidebar.header("**User Input** Sidebar")
+  st.sidebar.write("Dhiaka Shabrina Assyifa")
+  st.sidebar.write("A11.2020.13094")
 
   age = st.sidebar.number_input(label=":violet[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
