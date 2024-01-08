03d4abc50bb9ccebb9adc5f5a3c2e488f9104a25
Dhiaka Shabrina Assyifa <90568511+dhiaka54@users.noreply.github.com>
2024-01-08 09:37:09 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -87,6 +87,9 @@ smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
 
 model = pickle.load(open("model/xgb_model.pkl", 'rb'))
+model_info = pickle.load(open("model/xgb_best_model.pkl", 'rb'))
+print(type(model_info))
+
 
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
